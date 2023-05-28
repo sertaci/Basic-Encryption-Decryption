5b5b8016a1808c2bf1896535dd76c73f3d19929c
sertac ince <74237094+sertaci@users.noreply.github.com>
2023-05-28 18:00:37 +0300

    Update EtTuBrute.py

--- EtTuBrute.py
+++ EtTuBrute.py
@@ -5,7 +5,7 @@
 """
 Basic, dummy encryption based on Caesar cipher method.
 It uses basic keys such as 314, 271, 161, 662.
-these are the first 3 digits of some constants in the universe
+These are the first 3 digits of some constants in the universe.
 Pi, euler, golden ratio, and planck constant
 """
 
